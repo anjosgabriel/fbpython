d7e029a507845ca124df42eab84b542b57cfd198
unknown <anjosgabriel86@gmail.comm>
2024-05-06 21:51:41 -0300

    novos códigos

---
+++ orientadaObjetos/Cliente.py
@@ -0,0 +1,5 @@
+class Cliente:
+
+    def __init__(self, cpf, nome) -> None:
+        self.cpf = cpf
+        self.nome = nome
---
+++ orientadaObjetos/cadcliente.py
@@ -0,0 +1,7 @@
+import Cliente
+
+
+
+teste = Cliente("111.111.111-11", "MOISES")
+
+print(teste)
